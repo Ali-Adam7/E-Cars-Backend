07cd4307e36a1eeed62cc27ab03eb11e9b1299ed
Ali Adam <46465490+Ali-Adam7@users.noreply.github.com>
2024-05-18 23:59:47 -0400

    PM2
    PM2 for process management

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,5 +1,5 @@
 import fs from "fs";
 import path from "path";
 
-export const accessTokenKey = fs.readFileSync(path.resolve(__dirname, "../security/accessTokenKey.pem"), "utf8");
-export const refreshTokenKey = fs.readFileSync(path.resolve(__dirname, "../security/refreshTokenSecret.pem"), "utf8");
+export const accessTokenKey = fs.readFileSync(path.resolve(__dirname, "../keys/accessTokenKey.pem"), "utf8");
+export const refreshTokenKey = fs.readFileSync(path.resolve(__dirname, "../keys/refreshTokenSecret.pem"), "utf8");
